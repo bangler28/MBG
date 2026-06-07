185ecd6b554eccde94f9d417811b41f1f92b78f4
ER28 <untukebutuhan3@gmail.com>
2026-06-07 22:20:10 +0700

    Add initial HTML structure for index.php

---
+++ index.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Sistem Pendataan MBG</title>
+
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+
+<style>
+body{
+    background:#f5f9ff;
+}
+.hero{
+    margin-top:100px;
+}
+</style>
+
+</head>
+<body>
+
+<nav class="navbar navbar-dark bg-primary">
+<div class="container">
+<a class="navbar-brand" href="#">
+Sistem Pendataan MBG
+</a>
+</div>
+</nav>
+
+<div class="container hero">
+
+<div class="card shadow border-0">
+
+<div class="card-body text-center p-5">
+
+<h1 class="mb-3">
+Sistem Pendataan Sekolah Penerima MBG
+</h1>
+
+<p>
+Pendataan sekolah penerima program Makan Bergizi Gratis.
+</p>
+
+<a href="tambah.php" class="btn btn-primary">
+Tambah Data Sekolah
+</a>
+
+<a href="data_sekolah.php" class="btn btn-success">
+Lihat Data
+</a>
+
+</div>
+
+</div>
+
+</div>
+
+</body>
+</html>
